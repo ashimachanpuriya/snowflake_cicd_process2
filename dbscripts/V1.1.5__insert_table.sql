a943616f0fdaf16aeaa5e4f8b13aa1797c2f943c
ashimachanpuriya <chanpuriyaashima@gmail.com>
2023-02-09 15:58:04 +0530

    test - TESTING BRANCH

--- dbscripts/V1.1.5__insert_table.sql
+++ dbscripts/V1.1.5__insert_table.sql
@@ -5,3 +5,6 @@ VALUES (3, 'qwer', 'abc', 'abc3','Norway');
 
 INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
 VALUES (4, 'Ashi', 'neha', 'abc3','xyz1');
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (5, 'chanpuriya', 'NEHA', 'abc123','xyz123');
